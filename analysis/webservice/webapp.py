d31b33f3c14d60519f88f496161ece95e581efbb
Frank Greguska <francis.greguska@jpl.nasa.gov>
2018-01-11 10:43:40 -0800

    added apache license header to all py files

--- analysis/webservice/webapp.py
+++ analysis/webservice/webapp.py
@@ -1,3 +1,17 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
 Copyright (c) 2016 Jet Propulsion Laboratory,
 California Institute of Technology.  All rights reserved
@@ -74,6 +88,12 @@ class BaseHandler(tornado.web.RequestHandler):
     ''' Override me for standard handlers! '''
 
     def do_get(self, reqObject):
+
+        for root, dirs, files in os.walk("."):
+            for pyfile in [afile for afile in files if afile.endswith(".py")]:
+                print(os.path.join(root, pyfile))
+                with open(os.path.join(root, pyfile), 'r') as original: data = original.read()
+                with open(os.path.join(root, pyfile), 'w') as modified: modified.write(license + "\n" + data)
         pass
 
 
