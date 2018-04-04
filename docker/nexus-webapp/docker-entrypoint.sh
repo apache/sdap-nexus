1ce28accad6e6da98b69c18a0369a56b0eeb31f8
Frank Greguska <francis.greguska@jpl.nasa.gov>
2018-04-04 16:58:13 -0700

    added option for setting datacenter

--- docker/nexus-webapp/docker-entrypoint.sh
+++ docker/nexus-webapp/docker-entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-sed -i "s/server.socket_host.*$/server.socket_host=$SPARK_LOCAL_IP/g" /nexus/analysis/webservice/config/web.ini && \
-sed -i "s/cassandra1,cassandra2,cassandra3,cassandra4,cassandra5,cassandra6/$CASSANDRA_CONTACT_POINTS/g" /nexus/data-access/nexustiles/config/datastores.ini && \
-sed -i "s/solr1:8983/$SOLR_URL_PORT/g" /nexus/data-access/nexustiles/config/datastores.ini
+sed -i "s/server.socket_host.*$/server.socket_host=$SPARK_LOCAL_IP/g" /incubator-sdap-nexus/analysis/webservice/config/web.ini && \
+sed -i "s/cassandra1,cassandra2,cassandra3,cassandra4,cassandra5,cassandra6/$CASSANDRA_CONTACT_POINTS/g" /incubator-sdap-nexus/data-access/nexustiles/config/datastores.ini && \
+sed -i "s/local_datacenter=.*/local_datacenter=$CASSANDRA_LOCAL_DATACENTER/g" /incubator-sdap-nexus/data-access/nexustiles/config/datastores.ini && \
+sed -i "s/solr1:8983/$SOLR_URL_PORT/g" /incubator-sdap-nexus/data-access/nexustiles/config/datastores.ini
 
-cd /nexus/data-access
+cd /incubator-sdap-nexus/data-access
 python setup.py install --force
 
-cd /nexus/analysis
+cd /incubator-sdap-nexus/analysis
 python setup.py install --force
 
 python -m webservice.webapp
