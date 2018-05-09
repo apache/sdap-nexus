c24b6d462abda0e249dbb1ebafaa4153531ab673
Frank Greguska <francis.greguska@jpl.nasa.gov>
2018-05-08 17:03:42 -0700

    different env variable for tornado address.

--- docker/nexus-webapp/docker-entrypoint.sh
+++ docker/nexus-webapp/docker-entrypoint.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-sed -i "s/server.socket_host=.*/server.socket_host=$SPARK_LOCAL_IP/g" ${NEXUS_SRC}/analysis/webservice/config/web.ini && \
+if [ -n "$TORNADO_HOST" ]; then
+  sed -i "s/server.socket_host = .*/server.socket_host = '${TORNADO_HOST}'/g" ${NEXUS_SRC}/analysis/webservice/config/web.ini
+fi
 sed -i "s/host=127.0.0.1/host=$CASSANDRA_CONTACT_POINTS/g" ${NEXUS_SRC}/data-access/nexustiles/config/datastores.ini && \
 sed -i "s/local_datacenter=.*/local_datacenter=$CASSANDRA_LOCAL_DATACENTER/g" ${NEXUS_SRC}/data-access/nexustiles/config/datastores.ini && \
 sed -i "s/host=localhost:8983/host=$SOLR_URL_PORT/g" ${NEXUS_SRC}/data-access/nexustiles/config/datastores.ini
