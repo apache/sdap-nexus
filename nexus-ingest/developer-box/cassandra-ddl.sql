3121bd39d5b67d320dc3a4e5742a223c4a380f12
Frank Greguska <francis.greguska@jpl.nasa.gov>
2018-06-18 14:10:51 -0700

    removed obsolete code

--- nexus-ingest/developer-box/cassandra-ddl.sql
+++
@@ -1,6 +0,0 @@
-CREATE KEYSPACE IF NOT EXISTS nexustiles WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
-
-CREATE TABLE sea_surface_temp  (
-tile_id    uuid PRIMARY KEY,
-tile_blob  blob
-);
--- nexus-ingest/developer-box/start-singlenode-at-boot.sh
+++
@@ -1,4 +0,0 @@
-source activate nexus-xd-python-modules
-pushd /home/vagrant
-nohup ./spring-xd-1.3.1.RELEASE/xd/bin/xd-singlenode --hadoopDistro none > /vagrant/logs/xd-singlenode.log 2>&1 &
-popd
