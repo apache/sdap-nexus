7f1f7f0a2ea9abf8c9ca235a7bc0cd776bf7e49d
aqueric <antoine.queric@ifremer.fr>
2019-02-25 15:03:39 +0100

    SDAP-187 : fix typo

--- docker/nexus-webapp/standalone/docker-entrypoint.sh
+++ docker/nexus-webapp/standalone/docker-entrypoint.sh
@@ -21,14 +21,14 @@ if [ -n "$TORNADO_HOST" ]; then
   sed -i "s/server.socket_host = .*/server.socket_host = '${TORNADO_HOST}'/g" ${NEXUS_SRC}/analysis/webservice/config/web.ini
 fi
 sed -i "s/host=127.0.0.1/host=$CASSANDRA_CONTACT_POINTS/g" ${NEXUS_SRC}/data-access/nexustiles/config/datastores.ini && \
-sed -i "s/port=9042/host=$CASSANDRA_PORT/g" ${NEXUS_SRC}/data-access/nexustiles/config/datastores.ini && \
+sed -i "s/port=9042/port=$CASSANDRA_PORT/g" ${NEXUS_SRC}/data-access/nexustiles/config/datastores.ini && \
 sed -i "s/local_datacenter=.*/local_datacenter=$CASSANDRA_LOCAL_DATACENTER/g" ${NEXUS_SRC}/data-access/nexustiles/config/datastores.ini && \
 sed -i "s/host=localhost:8983/host=$SOLR_URL_PORT/g" ${NEXUS_SRC}/data-access/nexustiles/config/datastores.ini
 
 # DOMS
 sed -i "s/module_dirs=.*/module_dirs=webservice.algorithms,webservice.algorithms_spark,webservice.algorithms.doms/g" ${NEXUS_SRC}/analysis/webservice/config/web.ini && \
 sed -i "s/host=.*/host=$CASSANDRA_CONTACT_POINTS/g" ${NEXUS_SRC}/analysis/webservice/algorithms/doms/domsconfig.ini && \
- sed -i "s/port=.*/port=$CASSANDRA_PORT/g" ${NEXUS_SRC}/analysis/webservice/algorithms/doms/domsconfig.ini && \
+sed -i "s/port=.*/port=$CASSANDRA_PORT/g" ${NEXUS_SRC}/analysis/webservice/algorithms/doms/domsconfig.ini && \
 sed -i "s/local_datacenter=.*/local_datacenter=$CASSANDRA_LOCAL_DATACENTER/g" ${NEXUS_SRC}/analysis/webservice/algorithms/doms/domsconfig.ini
 
 cd ${NEXUS_SRC}/data-access
