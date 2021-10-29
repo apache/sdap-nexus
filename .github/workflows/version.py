a9d2c67dfd98ba5b9bc9e4dfe6e266bde3f6ebcc
Joshua Garde <joshgarde@gmail.com>
2021-10-29 13:08:58 -0700

    Lint version.py

--- .github/workflows/version.py
+++ .github/workflows/version.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser
-import os
-import re
 
-version_regex = re.compile('(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)((a(?P<alpha_num>\d+))|(-(?P<commit>.*)))?')
+version_regex = r'(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)((a(?P<alpha_num>\d+))|(-(?P<commit>.*)))?'  # noqa: E501
+
 
 def main():
     parser = ArgumentParser()
@@ -17,16 +16,18 @@ def main():
     version_contents = version_file.read()
 
     current_version = version_regex.search(version_contents)
-    if current_version == None:
+    if current_version is None:
         print('Version not found in file')
         exit(1)
 
     if args.phase == 'current':
         print(current_version.group(0))
+        version_file.close()
         return
 
     new_version = bump_version(current_version, args.phase, args.value)
-    version_contents, _ = version_regex.subn(new_version, version_contents, count=1)
+    version_contents = version_regex.sub(
+        new_version, version_contents, count=1)
 
     print(new_version)
 
@@ -35,18 +36,25 @@ def main():
     version_file.truncate()
     version_file.close()
 
+
 def bump_version(version, phase, value):
     major = int(version.group('major'))
     minor = int(version.group('minor'))
     patch = int(version.group('patch'))
 
-    if phase == 'pre-alpha':
+    if phase == 'manual':
+        if value == 'auto':
+            raise Exception('value cannot be auto on manual')
+
+        return value
+    elif phase == 'pre-alpha':
         if value == 'auto':
             raise Exception('value cannot be auto on pre-alpha')
 
         return f'{major}.{minor}.{patch}-{value}'
     elif phase == 'alpha':
-        alpha_num = 0 if version.group('alpha_num') == None else int(version.group('alpha_num')) + 1
+        alpha_num = version.group('alpha_num')
+        alpha_num = 0 if alpha_num is None else int(alpha_num) + 1
 
         return f'{major}.{minor}.{patch}a{alpha_num}'
     elif phase == 'patch':
