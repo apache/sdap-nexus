c9c1849d037668d8e376ba27f8983b0795acb558
skorper <stepheny.k.perez@gmail.com>
2022-10-14 15:26:24 -0700

    Added second NCAR provider that uses JPL Insitu API

--- analysis/webservice/algorithms/doms/insitu.py
+++ analysis/webservice/algorithms/doms/insitu.py
@@ -38,6 +38,7 @@ def query_insitu(dataset, variable, start_time, end_time, bbox, platform, depth_
         pass
 
     provider = insitu_endpoints.get_provider_name(dataset)
+    project = insitu_endpoints.get_project_name(dataset)
 
     params = {
         'itemsPerPage': items_per_page,
@@ -47,7 +48,7 @@ def query_insitu(dataset, variable, start_time, end_time, bbox, platform, depth_
         'minDepth': depth_min,
         'maxDepth': depth_max,
         'provider': provider,
-        'project': dataset,
+        'project': project,
         'platform': platform,
     }
 
@@ -57,7 +58,7 @@ def query_insitu(dataset, variable, start_time, end_time, bbox, platform, depth_
     insitu_response = {}
 
     # Page through all insitu results
-    next_page_url = insitu_endpoints.getEndpoint(provider)
+    next_page_url = insitu_endpoints.getEndpoint(provider, dataset)
     while next_page_url is not None and next_page_url != 'NA':
         if session is not None:
             response = session.get(next_page_url, params=params)
