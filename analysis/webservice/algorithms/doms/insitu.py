ececbd0f60babd0bfde4a95428368d43d2242be5
rileykk <rileykk@jpl.nasa.gov>
2022-10-25 15:03:02 -0700

    Made timeout variables

--- analysis/webservice/algorithms/doms/insitu.py
+++ analysis/webservice/algorithms/doms/insitu.py
@@ -9,6 +9,11 @@ from urllib.parse import urlencode
 from webservice.webmodel import NexusProcessingException
 
 
+CONNECT_TIMEOUT = 9.05     # Recommended to be just above a multiple of 3 seconds
+READ_TIMEOUT = 303          # Just above current gateway timeout
+TIMEOUTS = (CONNECT_TIMEOUT, READ_TIMEOUT)
+
+
 def query_insitu_schema():
     """
     Query the "cdms_schema" insitu endpoint. This will return the JSON
@@ -18,7 +23,7 @@ def query_insitu_schema():
     schema_endpoint = insitu_endpoints.getSchemaEndpoint()
     logging.info("Querying schema")
     try:
-        response = requests.get(schema_endpoint, timeout=(15.05, 331))
+        response = requests.get(schema_endpoint, timeout=TIMEOUTS)
     except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectTimeout):
         raise NexusProcessingException(code=504, reason=f'Insitu schema request timed out')
 
@@ -75,9 +80,9 @@ def query_insitu(dataset, variable, start_time, end_time, bbox, platform, depth_
 
         try:
             if session is not None:
-                response = session.get(next_page_url, params=params, timeout=(15.05, 331))
+                response = session.get(next_page_url, params=params, timeout=TIMEOUTS)
             else:
-                response = requests.get(next_page_url, params=params, timeout=(15.05, 331))
+                response = requests.get(next_page_url, params=params, timeout=TIMEOUTS)
         except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectTimeout):
             raise NexusProcessingException(code=504, reason=f'Insitu request timed out after {str(datetime.now() - thetime)} seconds')
 
