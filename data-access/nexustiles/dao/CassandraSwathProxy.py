Fix for swath masking

data-access/nexustiles/dao/CassandraSwathProxy.py
@@ -112,8 +112,8 @@ class NexusTileData(Model):
             time_data = np.ma.masked_invalid(from_shaped_array(swath_tile.time))
 
             # Simplify the tile if the time dimension is the same value repeated
-            if np.all(time_data == np.min(time_data)):
-                time_data = np.array([np.min(time_data)])
+            # if np.all(time_data == np.min(time_data)):
+            #     time_data = np.array([np.min(time_data)])
 
             swath_tile_data = np.ma.masked_invalid(from_shaped_array(swath_tile.variable_data))
             tile_data = swath_tile_data
@@ -161,8 +161,8 @@ class NexusTileData(Model):
             time_data = np.ma.masked_invalid(from_shaped_array(swath_tile.time))
 
             # Simplify the tile if the time dimension is the same value repeated
-            if np.all(time_data == np.min(time_data)):
-                time_data = np.array([np.min(time_data)])
+            # if np.all(time_data == np.min(time_data)):
+            #     time_data = np.array([np.min(time_data)])
 
             swath_tile_data = np.ma.masked_invalid(from_shaped_array(swath_tile.variable_data))
             swath_tile_data = np.moveaxis(swath_tile_data, -1, 0)