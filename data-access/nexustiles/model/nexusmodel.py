0c0fcb32cbf0ebb9acdeb3ba1ce301d7bf6f1554
rileykk <rileykk@jpl.nasa.gov>
2023-03-31 13:15:56 -0700

    Commented out debug print statements

--- data-access/nexustiles/model/nexusmodel.py
+++ data-access/nexustiles/model/nexusmodel.py
@@ -129,13 +129,13 @@ class Tile(object):
     def nexus_point_generator(self, include_nan=False):
         indices = self.get_indices(include_nan)
 
-        print(indices)
+        # print(indices)
 
         idx_len = len(indices[0])
 
-        print(self.latitudes)
-        print(self.times)
-        print(self.data)
+        # print(self.latitudes)
+        # print(self.times)
+        # print(self.data)
 
         time_slice = slice(0, 1) if idx_len == 3 else slice(None)
         geo_slice = slice(-1, None) if idx_len == 3 else slice(None)
