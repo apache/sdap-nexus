440 - Fixed masking and grid handling

data-access/nexustiles/dao/CassandraSwathProxy.py
@@ -91,8 +91,10 @@ class NexusTileData(Model):
             reflected_lat_array = np.broadcast_to(latitude_data, (len(longitude_data), len(latitude_data)))
             reflected_lat_array = np.transpose(reflected_lat_array)
 
-            if len(grid_tile_data.shape) == 2:
-                grid_tile_data = grid_tile_data[np.newaxis, :]
+            time_array = np.broadcast_to(grid_tile.time, grid_tile_data.shape)
+
+            # if len(grid_tile_data.shape) == 2:
+            #     grid_tile_data = grid_tile_data[np.newaxis, :]
 
             # Extract the meta data
             meta_data = {}
@@ -103,7 +105,7 @@ class NexusTileData(Model):
                     meta_array = meta_array[np.newaxis, :]
                 meta_data[name] = meta_array
 
-            return reflected_lat_array, reflected_lon_array, np.array([grid_tile.time]), grid_tile_data, meta_data, is_multi_var
+            return reflected_lat_array, reflected_lon_array, time_array, grid_tile_data, meta_data, is_multi_var
         elif self._get_nexus_tile().HasField('swath_tile'):
             swath_tile = self._get_nexus_tile().swath_tile
 
@@ -180,7 +182,7 @@ class NexusTileData(Model):
                 actual_meta_array = np.ma.masked_invalid(from_shaped_array(meta_data_obj.meta_data))
                 meta_data[name] = actual_meta_array
 
-            return latitude_data, longitude_data, time_data, tile_data, meta_data, is_multi_var
+            return latitude_data, longitude_data, time_data, np.ma.array(tile_data), meta_data, is_multi_var
         elif self._get_nexus_tile().HasField('grid_multi_variable_tile'):
             grid_multi_variable_tile = self._get_nexus_tile().grid_multi_variable_tile
             is_multi_var = True

data-access/nexustiles/model/nexusmodel.py
@@ -175,7 +175,7 @@ class Tile(object):
         if include_nan:
             return list(np.ndindex(self.data.shape))
         if self.is_multi:
-            combined_data_inv_mask = reduce(np.logical_and, [data.mask for data in self.data])
+            combined_data_inv_mask = reduce(np.logical_and, [np.ma.getmaskarray(data) for data in self.data])
             return np.argwhere(np.logical_not(combined_data_inv_mask))
         else:
             return np.transpose(np.where(np.ma.getmaskarray(self.data) == False)).tolist()

data-access/nexustiles/nexustiles.py
@@ -395,7 +395,10 @@ class NexusTileService(object):
             # If this is multi-var, need to mask each variable separately.
             if tile.is_multi:
                 # Combine space/time mask with existing mask on data
-                data_mask = reduce(np.logical_or, [tile.data[0].mask, data_mask])
+                # Data masks are ANDed because we want to mask out only when ALL data vars are invalid
+                combined_data_mask = reduce(np.logical_and, [d.mask for d in tile.data])
+                # We now OR in the bounds mask because out of bounds data must be excluded regardless of validity
+                data_mask = np.logical_or(combined_data_mask, data_mask)
 
                 num_vars = len(tile.data)
                 multi_data_mask = np.repeat(data_mask[np.newaxis, ...], num_vars, axis=0)